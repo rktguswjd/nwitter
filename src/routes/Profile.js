87b4a91168ec43fb51eb9a5b254aeb41142e3137
rktguswjd <010_cm@naver.com>
2020-10-27 16:43:08 +0900

    5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,10 @@
 import { authService, dbService } from "fbase";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 export default ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
@@ -21,8 +22,32 @@ export default ({ userObj }) => {
     getMyNweets();
   }, []);
 
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Udate Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out </button>
     </>
   );
