973c159624031219db394c273f86ebe17f92d759
rktguswjd <010_cm@naver.com>
2020-10-28 18:25:09 +0900

    Updates

---
+++ precache-manifest.24db11a6297a41a76ac6dfb22a2de4d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cbb1716a46c223ef1e77fdbe86c4ab",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "383fc17e301cf38666f6",
+    "url": "/nwitter/static/css/main.da5830ff.chunk.css"
+  },
+  {
+    "revision": "ad7f54e340237bb5d89b",
+    "url": "/nwitter/static/js/2.10a8f478.chunk.js"
+  },
+  {
+    "revision": "954d88908e6b1f345323e3bc51916644",
+    "url": "/nwitter/static/js/2.10a8f478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383fc17e301cf38666f6",
+    "url": "/nwitter/static/js/main.6fcf4771.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
